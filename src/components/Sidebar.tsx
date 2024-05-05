7e1c779b0fa63f36b4dd862b31b4a766bf6faa54
IslandBoyDev <47767271+Chezo25@users.noreply.github.com>
2024-05-04 22:42:07 -0400

    add

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between
-    bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>Sidebar</section>  
+    bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+      Sidebar
+      </section>  
   )
 }
 
