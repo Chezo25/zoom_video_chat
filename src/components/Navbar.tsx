3fd3b3c304bfc02a661e926924aa9af7fcd9bd01
IslandBoyDev <47767271+Chezo25@users.noreply.github.com>
2024-05-07 21:42:42 -0400

    start wuk on mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
@@ -13,7 +14,15 @@ const Navbar = () => {
         alt='Mungo logo'
         className='max-sm:size-10'
         />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Mungo</p>
       </Link>
+
+      <div className="flex-between gap-5">
+        {/* Clerk Manager */}
+
+        <MobileNav />
+      </div>
       </nav>
   )
 }
