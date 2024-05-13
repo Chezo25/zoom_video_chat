2d2cbec1bbf296d23f3c498517821fbdcc493c65
IslandBoyDev <47767271+Chezo25@users.noreply.github.com>
2024-05-12 20:01:07 -0400

    burger content

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image'
+  
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <section className='w-full max-w-[264px]'>
+                <Sheet>
+         <SheetTrigger asChild>
+            <Image 
+            src='/public/icons/hamburger.svg'
+            width={36}
+            height={36}
+            alt='hamburger icon'
+            className='cursor-pointer sm:hidden'
+            />
+         </SheetTrigger>
+         <SheetContent>
+         <SheetHeader>
+             <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+            </SheetDescription>
+            </SheetHeader>
+        </SheetContent>
+        </Sheet>
+
+    </section>
   )
 }
 
