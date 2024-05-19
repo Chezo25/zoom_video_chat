6be3204179583e533efdcf0dd21dad79934516b7
IslandBoyDev <47767271+Chezo25@users.noreply.github.com>
2024-05-18 22:52:13 -0400

    finish boilplate code

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,18 +1,18 @@
+"use client"
+
 import React from 'react'
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
+    Sheet,SheetClose,SheetContent,SheetTrigger,
   } from "@/components/ui/sheet"
 import Image from 'next/image'
 import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
   
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
     <section className='w-full max-w-[264px]'>
                 <Sheet>
@@ -40,7 +40,33 @@ const MobileNav = () => {
 
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="flex h-full flex-col gap-6 pt-16 text-white"></section>
+              <section className="flex h-full flex-col gap-6 pt-16 text-white">
+                          {sidebarLinks.map((link) => {
+                      const isActive = pathname === link.route; 
+
+                      // Render the link with appropriate styling based on isActive
+                      return (
+                        <SheetClose asChild key={link.route}>
+                      <Link href = {link.route} key={link.title}
+                        className={cn('flex gap-4 items-center p-4 rounded-lg w-full max-w-60', 
+                        {'bg-blue-1': isActive,
+                          
+                        })}
+                        >
+                          <Image
+                          src={link.iconImg} 
+                          alt={link.title}
+                          width={20}
+                          height={20}
+                          />
+                          <p className='font-semibold'>
+                            {link.title}
+                          </p>
+                        </Link>
+                        </SheetClose>
+                      );
+                    })}
+              </section>
             </SheetClose>
           </div>
 
